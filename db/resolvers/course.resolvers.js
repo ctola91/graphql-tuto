1c985ce6a7e81d5aa87d3ebcf61efaa545eee3fd
cmtp <christian.tola@hotmail.com>
2019-07-29 23:55:34 -0400

    add database with mongodb

--- db/resolvers/course.resolvers.js
+++ db/resolvers/course.resolvers.js
@@ -1,16 +1,17 @@
 const Course = require("../models/course");
 
-let courses = [];
-
 module.exports = {
   Query: {
     async getCourses(obj, { page, limit }) {
-      const courses = await Course.find();
-      return courses;
+      let courses = Course.find();
+      if (page !== undefined) {
+          courses = courses.limit(limit).skip((page -1) * limit)
+      }
+      return await courses;
     },
     async getCourse(obj, { id }) {
-        const course = await Course.findById(id);
-        return course;
+      const course = await Course.findById(id);
+      return course;
     }
   },
   Mutation: {
@@ -19,15 +20,12 @@ module.exports = {
       await course.save();
       return course;
     },
-    updateCourse(obj, { id, input }) {
-      const courseIndex = courses.findIndex(course => course.id === id);
-      const course = courses[courseIndex];
-
-      const newCourse = Object.assign(course, input);
-      return newCourse;
+    async updateCourse(obj, { id, input }) {
+      const course = await Course.findByIdAndUpdate(id, input);
+      return course;
     },
-    deleteCourse(obj, { id }) {
-      courses = courses.filter(course => course.id !== id);
+    async deleteCourse(obj, { id }) {
+      await Course.deleteOne({ _id: id });
       return {
         message: `Course with id: ${id} was deleted`
       };
